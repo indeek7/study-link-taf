76b02338215f9bd66a035dab270d432748874e22
indeek7 <indee.k7@gmail.com>
2023-04-08 08:59:45 +0930

    Add comments

--- cypress/lib/functions/common/NavigationFunctions.js
+++ cypress/lib/functions/common/NavigationFunctions.js
@@ -1,16 +1,28 @@
 import StudyLinkConfigs from '../../../config/StudyLinkConfigs.json';
 
+/**
+ * Open Solutions page by passing url
+ * 
+ */
 export const openSolutionsPage = () => {
     cy.visit(StudyLinkConfigs.baseUrl + StudyLinkConfigs.solutionsPageUrl);
     cy.title().should('include', 'Solutions – connect.studylink'); 
 };
 
+/**
+ * Open Partner Portal page by passing url
+ * 
+ */
 export const openPartnerPotalPage = () => {
     cy.visit(StudyLinkConfigs.baseUrl + StudyLinkConfigs.partnerPortalPageUrl);
     cy.xpath('.//a[@class="uabb-tab-link"]/span[contains(text(), "The challenge")]', { timeout: Cypress.config('longCommandTimeout') }).should('be.visible');
     cy.title().should('include', 'For Agents – connect.studylink'); 
   };
 
+  /**
+   * Open Capture product page
+   * 
+   */
   export const openCapturePage = () => {
     cy.visit(StudyLinkConfigs.baseUrl + StudyLinkConfigs.capturePageUrl);
     cy.title().should('include', 'Capture – connect.studylink'); 
